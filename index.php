ae2bd28a39e567b461121039dad9d17b0347dd6b
substanceswe <126781108+substanceswe@users.noreply.github.com>
2023-03-03 18:12:57 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Steam Link Generator - ALPHA v0.1</title>
+    <title>Steam Link Generator - ALPHA v0.2</title>
     <style>
         body {
             background-color: #f4f4f4;
@@ -72,7 +72,7 @@
     </style>
 </head>
 <body>
-    <h1>Steam Link Generator - ALPHA v0.1</h1>
+    <h1>Steam Link Generator - ALPHA v0.2</h1>
     <form method="post">
         <label for="app_names">Enter app names (separated by new row):</label>
         <br>
@@ -101,27 +101,62 @@ if(isset($_POST['submit'])){
     $app_list_array = json_decode($app_list_json, true);
     $app_list = $app_list_array['applist']['apps'];
 
+    $app_names_array = preg_split('/\r\n|\r|\n/', $app_names);
+    $app_list = array_combine(array_column($app_list, 'name'), $app_list);
+    $app_list_lower = array_change_key_case($app_list, CASE_LOWER);
+    
     $table = "<table border='1'><tr><th>#</th><th>Game</th><th>Thumbnail</th></tr>";
     $num = 1;
     foreach ($app_names_array as $app_name) {
         $app_name = trim($app_name);
         $app_id = '';
-        foreach ($app_list as $app) {
-            if (strtolower($app_name) == strtolower($app['name'])) {
-                $app_id = $app['appid'];
-                break;
+        $best_match = null;
+        $exact_match = null;
+    
+        // Convert the app name to lowercase to perform case-insensitive matching
+        $app_name_lower = strtolower($app_name);
+    
+        //Explicit exceptions
+        if ($app_name_lower == ">observer_") {
+            $app_name_lower = "observer";
+        }
+
+        // Look for an exact match in the pre-processed app list
+        if (isset($app_list_lower[$app_name_lower])) {
+            $exact_match = $app_list_lower[$app_name_lower];
+        } else {
+            // Look for a fuzzy match using a combination of different similarity metrics and thresholds
+            foreach ($app_list_lower as $app_lower) {
+                $name_similarity = similar_text($app_name_lower, strtolower($app_lower['name']), $name_similarity_percentage);
+                $name_distance = levenshtein($app_name_lower, strtolower($app_lower['name']));
+    
+                if ($name_similarity_percentage >= 90 || $name_distance <= 3) {
+                    $best_match = $app_lower;
+                    break;
+                } elseif ($name_similarity_percentage >= 75 || $name_distance <= 5) {
+                    if (!$best_match || ($name_similarity_percentage > similar_text($app_name_lower, strtolower($best_match['name']), $best_match_similarity_percentage) && $name_distance < levenshtein($app_name_lower, strtolower($best_match['name'])))) {
+                        $best_match = $app_lower;
+                    }
+                }
             }
         }
-        if (empty($app_id)) {
+    
+        if ($exact_match) {
+            $app_id = $exact_match['appid'];
+            $game_name = "<a href='https://store.steampowered.com/app/{$app_id}/{$app_name}/'>" . ucwords($exact_match['name']) . "</a><br>";
+            $thumbnail_url = "https://steamcdn-a.akamaihd.net/steam/apps/{$app_id}/capsule_184x69.jpg";
+        } elseif ($best_match) {
+            $app_id = $best_match['appid'];
+            $game_name = "<a href='https://store.steampowered.com/app/{$app_id}/{$app_name}/'>" . ucwords($best_match['name']) . "</a><br>";
+            $thumbnail_url = "https://steamcdn-a.akamaihd.net/steam/apps/{$app_id}/capsule_184x69.jpg";
+        } else {
             $game_name = ucwords($app_name) . " (Not found on Steam)";
             $thumbnail_url = "https://via.placeholder.com/184x69?text=No+Thumbnail";
-        } else {
-            $game_name = "<a href='https://store.steampowered.com/app/{$app_id}/{$app_name}/'>" . ucwords($app_name) . "</a><br>";
-            $thumbnail_url = "https://steamcdn-a.akamaihd.net/steam/apps/{$app_id}/capsule_184x69.jpg";
         }
+    
         $table .= "<tr><td>$num</td><td class='game'>$game_name</td><td><a href='https://store.steampowered.com/app/{$app_id}/{$app_name}/'><img src='$thumbnail_url' alt='Thumbnail for $app_name'></a></td></tr>";
         $num++;
-    }        
+    }
     $table .= "</table>";
     echo $table;
 }
